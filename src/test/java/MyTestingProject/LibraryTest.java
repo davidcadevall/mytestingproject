b686565727ff194e89ac89a81580920be1c21f86
carol11811 <carol11811@gmail.com>
2020-10-06 11:41:11 +0200

    Prueba1

--- src/test/java/MyTestingProject/LibraryTest.java
+++ src/test/java/MyTestingProject/LibraryTest.java
@@ -11,4 +11,11 @@ public class LibraryTest {
         Library classUnderTest = new Library();
         assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
     }
+
+
+
+    @Test public void testSomeLibraryMethodtwo() {
+        Library classUnderTest = new Library();
+        assertFalse("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+    }
 }
