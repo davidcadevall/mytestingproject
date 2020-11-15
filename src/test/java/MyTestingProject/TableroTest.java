0483f18728280eb33268faedc7734c9f9369bfec
carol11811 <carol11811@gmail.com>
2020-11-15 22:51:32 +0100

    Fixed bugs

--- src/test/java/MyTestingProject/TableroTest.java
+++ src/test/java/MyTestingProject/TableroTest.java
@@ -14,6 +14,8 @@ public class TableroTest {
 	@Test
 	public void testTablero() {
 		//Assert de created 
+		System.out.println(" ---------------------------------------- ");
+		System.out.println(" INICIO TEST TABLERO ");
 		
 		//Probando maximos 
 		Tablero t = new Tablero(99,5,true,1);
@@ -40,7 +42,8 @@ public class TableroTest {
 	@Test
 	public void testGetHorizontal() {
 		
-		
+		System.out.println(" ---------------------------------------- ");
+		System.out.println(" INICIO TEST GET HORIZONTAL ");
 		Tablero t = new Tablero(7,6,true,10);
 		int res_o=t.getHorizontal();
 		assertEquals(res_o,7);
@@ -48,15 +51,21 @@ public class TableroTest {
 
 	@Test
 	public void testGetVertical() {
+		System.out.println(" ---------------------------------------- ");
+		System.out.println(" INICIO TEST GET VERTICAL ");
 		Tablero t = new Tablero(8,6,true,10);
 		int res_o=t.getVertical();
 		assertEquals(res_o,6);
 	}
 	@Test
 	public void testGetCreated() {
+		System.out.println(" ---------------------------------------- ");
+		System.out.println(" INICIO TEST GETCREATED ");
+		// CREA TABLERO
 		Tablero t = new Tablero(6,6,true,10);
 		boolean res_o=t.getCreated();
 		assertEquals(res_o,true);
+		// NO CREA TABLERO
         t = new Tablero(0,0,true,10);
 		res_o=t.getCreated();
 		assertEquals(res_o,false);
